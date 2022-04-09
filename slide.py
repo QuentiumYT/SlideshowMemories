7c788e7f908242c93ab2624f9a765cf843a9adb6
QuentiumYT <quentin.lienhardt@gmail.com>
2022-04-09 18:52:15 +0200

    Retrieve image location using positionstack API

--- slide.py
+++ slide.py
@@ -1,4 +1,4 @@
-import os, random
+import os, dotenv, requests, random
 import tkinter as tk
 from PIL import Image, ImageTk, ExifTags
 from datetime import datetime
@@ -65,13 +65,43 @@ class SlideShow(tk.Tk):
         """
         Parse date using exif data if exists, else use file modification date
         """
-
         if image_date:
             date = datetime.strptime(image_date, "%Y:%m:%d %H:%M:%S")
         else:
             date = datetime.fromtimestamp(os.stat(image_path).st_mtime)
 
         return date.strftime("%d/%m/%Y %H:%M:%S")
+
+    def get_image_coords(self, gps_info: dict) -> str:
+        """
+        Get the image coordinates as latitude, longitude and altitude
+        """
+        # Convert the GPS coordinates stored in the EXIF to degrees in float format
+        dms_to_decimal = lambda dms: float(dms[0]) + float(dms[1]) / 60 + float(dms[2]) / 3600
+
+        coords_data = {ExifTags.GPSTAGS[k]: v for k, v in gps_info.items() if k in ExifTags.GPSTAGS}
+
+        if coords_data:
+            lat = + dms_to_decimal(coords_data.get("GPSLatitude"))
+            lon = + dms_to_decimal(coords_data.get("GPSLongitude"))
+            alt = round(coords_data.get("GPSAltitude"))
+
+            if coords_data.get("GPSLatitudeRef") != "N":
+                lat *= -1
+            if coords_data.get("GPSLongitudeRef") != "E":
+                lon *= -1
+
+        return lat, lon, alt
+
+    def get_image_location(self, lat, lon):
+        """
+        Get the place at which the image was taken using latitude and longitude
+        """
+        key = os.environ.get("API_KEY")
+        url = f"http://api.positionstack.com/v1/reverse?access_key={key}&query={lat},{lon}&limit=1"
+        req = requests.get(url)
+
+        return req.json()["data"][0]["name"] + ", " + req.json()["data"][0]["locality"]
 
     def show_image(self, filepath: str):
         """
@@ -80,22 +110,36 @@ class SlideShow(tk.Tk):
         image = Image.open(filepath)
 
         filename = filepath.split(os.sep)[-1]
-        filedata = self.parse_image_data(image)
-        filedate = self.get_image_date(filepath, filedata.get("DateTimeOriginal", filedata.get("DateTime")))
+        image_data = self.parse_image_data(image)
+        image_date = self.get_image_date(filepath, image_data.get("DateTimeOriginal", image_data.get("DateTime")))
+        if image_data.get("GPSInfo"):
+            image_coords = self.get_image_coords(image_data.get("GPSInfo"))
+            image_alt = "Altitude : " + str(image_coords[2]) + "m"
+            image_loc = self.get_image_location(image_coords[0], image_coords[1])
+        else:
+            image_coords = (0, 0, 0)
+            image_alt = None
+            image_loc = "Lieu non défini"
 
         image.thumbnail((self.screen_w, self.screen_h), Image.Resampling.LANCZOS)
 
         self.current_image = ImageTk.PhotoImage(image)
         self.canvas.delete("all")
         self.canvas.create_image(self.screen_w / 2, self.screen_h / 2, anchor="center", image=self.current_image)
-        # Image name top left
-        self.canvas.create_text(20, 10, text=filename, fill="white", font=("Ubuntu", 12), anchor="nw")
-        # Image date bottom left
-        self.canvas.create_text(20, self.screen_h - 10, text=filedate, fill="white", font=("Ubuntu", 12), anchor="sw")
+
+        # Image date top left
+        self.canvas.create_text(20, 10, text=image_date, fill="white", font=("Ubuntu", 12), anchor="nw")
+        # Image location bottom left
+        self.canvas.create_text(20, self.screen_h - (30 if image_alt else 10), text=image_loc, fill="white", font=("Ubuntu", 12), anchor="sw")
+        self.canvas.create_text(20, self.screen_h - 10, text=image_alt, fill="white", font=("Ubuntu", 12), anchor="sw")
+        # Image name bottom right
+        self.canvas.create_text(self.screen_w - 20, self.screen_h - 10, text=filename, fill="white", font=("Ubuntu", 12), anchor="se")
 
 
 
 if __name__ == "__main__":
+    dotenv.load_dotenv()
+
     slideShow = SlideShow(directory="img/")
     slideShow.start_slideshow(delay=2)
     slideShow.mainloop()
